cdc2cfd10069d14fb7bbc7f8fb763996497208e3
Rhystalgie <aufamuh12@gmail.com>
2024-12-20 03:17:49 +0700

    my_pills

--- lib/pages/my_pills.dart
+++ lib/pages/my_pills.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_theraphy/widgets/bottom_navigation_bar.dart';
 
+class MyPillsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('My Pills'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: const Center(
+              child: Text("Halaman My Pills"),
+            ),
+          ),
+          CustomBottomNavigationBar(),
+        ],
+      ),
+    );
+  }
+}
